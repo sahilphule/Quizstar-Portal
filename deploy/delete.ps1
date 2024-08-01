081cd197fea03d76c6cbd6015e7c40eb16d0206e
unknown <sahilphule2003@gmail.com>
2024-08-01 16:18:09 +0530

    modified pull policy and pv mount location for mysql

--- deploy/delete.ps1
+++ deploy/delete.ps1
@@ -10,17 +10,18 @@ kubectl delete service/nginx-service -n quizstar
 kubectl delete deployment/django-app-deployment -n quizstar
 kubectl delete service/django-app-service -n quizstar
 kubectl delete pvc django-app-pvc -n quizstar
-kubectl delete pv django-app-pv
+kubectl delete pv django-app-pv -n quizstar
 
 kubectl delete deployment/mysql-deployment -n quizstar
 kubectl delete service/mysql-service -n quizstar
 kubectl delete pvc mysql-pvc -n quizstar
-kubectl delete pv mysql-pv
+kubectl delete pv mysql-pv -n quizstar
 
 kubectl delete configmap app-configmap -n quizstar
 kubectl delete secret app-secret -n quizstar
+kubectl delete secret docker-secret -n quizstar
 
 kubectl get all -n quizstar
 kubectl get pvc -n quizstar
-kubectl get pv
+kubectl get pv -n quizstar
 kubectl get secrets -n quizstar
--- django-app/entrypoint.sh
+++ django-app/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('sahil', 'admin@myproject.com', '123456')" | python manage.py shell
 # python manage.py runserver 0.0.0.0:8000
 python manage.py collectstatic --no-input
 gunicorn core.wsgi:application --bind 0.0.0.0:8000
